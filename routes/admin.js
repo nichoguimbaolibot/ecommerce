9a23f2e45ca5eba90b71eb2897aa9648803d8e1f
nicho <nguimbaolibot.chrs@gmail.com>
2017-09-30 16:11:27 +0800

    add model category, and product, added routes and views for admin

---
+++ routes/admin.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var router = express.Router();
+var Category = require("../models/category");
+
+
+
+router.get("/add-category", function(req, res, next){
+	res.render("admin/add-category", {message: req.flash("success")});
+});
+
+
+router.post("/add-category", function(req, res, next){
+	var category = new Category();
+	category.name = req.body.name;
+
+	category.save(function(err, category){
+		if(err) return next(err);
+
+		req.flash("success", "Successfully added a category");
+		return res.redirect("/add-category");
+	});
+});
+
+
+module.exports = router;
