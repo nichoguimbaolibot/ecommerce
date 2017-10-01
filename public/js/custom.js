d5bbc43be854ef1f6131d0090089ecf6a9010b81
nicho <nguimbaolibot.chrs@gmail.com>
2017-10-01 18:17:58 +0800

    added glypicon fonts and added instant search on custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,5 @@
 $(function(){
-	$("#search").keyup(function(){
+	$("#search").keypress(function(){
 		var search_term = $(this).val();
 
 		$.ajax({
@@ -13,6 +13,23 @@ $(function(){
 				var data = json.hits.hits.map(function(hit){
 			return hit;
 			});
+				$("#searchResults").empty();
+				for(var i = 0; i < data.length; i++){
+					var html = "";
+					html += '<div class="col-md-4">';
+					html+= '<a href="/product/' +  data[i]._source._id  + '">';
+					html+= '<div class="thumbnail">';
+					html+= '<img src="' +  data[i]._source.image  + '">';
+					html+= '<div class="caption">';
+					html+= '<h3>' +  data[i]._source.name  + '</h3>';
+					html+= '<p>' +  data[i]._source.category.name  + '</p>';
+					html+= '<p>$' +  data[i]._source.price  + '</p>';
+					html+= '</div>';
+					html+= '</div>';
+					html+= '</a>';
+					html+= '</div>';
+					$("#searchResults").append(html);				
+				}
 			},
 			error: function(err){
 				console.log(err);
